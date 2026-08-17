5f2be49241e27280faabaf0c47cfe3fd324d76c7
Lakshmi Tanmay <lakshmi@voltcrash.com>
2026-08-17 12:20:56 +0530

    chore: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+import adapter from "@sveltejs/adapter-vercel";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig, lazyPlugins } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  plugins: lazyPlugins(() => [
+    tailwindcss(),
+    sveltekit({
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) =>
+          filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+      },
+      adapter: adapter(),
+    }),
+  ]),
+});
